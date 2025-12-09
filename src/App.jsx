2fdb34a273b35fd6761d405f9f89f5e92f5c4f38
Aymern_noorjr <usmanadamuusman94@gmail.com>
2025-12-09 14:09:31 +0100

    feat

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Hero from "./components/hero/Hero";
-import Navbar from "./components/shared/Navbar";  
-import Footer from "./components/shared/Footer";
+import Navbar from "./components/shared/Navbar";
+import Achiever from "./components/home/achiever";  
+import Footer from "./components/shared/Footer";  
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Achiever />
       <Footer />
     </div>
   );
